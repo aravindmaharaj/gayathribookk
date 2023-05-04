5d2a1fcf0d7e6e4bdb8dda3cc064b2496a6d6648
aravindmaharaj <aravindneruvatla22@gmail.com>
2023-05-04 11:43:32 +0530

    js link

---
+++ js/jsgoogle.js
@@ -0,0 +1,21 @@
+// <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-X9W7YLPMF0"></script>
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-X9W7YLPMF0');
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9493323507160233"
+     crossorigin="anonymous"></script>
+     
+      function myFunction() {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
+      
